142b57beb1c4d24788b6381319525af730c4f956
lbertoncello <lucas_b_andrade@hotmail.com>
2021-09-28 15:12:26 -0300

    fix: collectCoverageFrom change from string to array

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: '<rootDir>/src/**/*.ts',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
