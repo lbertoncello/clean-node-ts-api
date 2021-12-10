ac0f62068c785db72bbcd3eebbaa999895e5b874
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-10 18:53:03 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,29 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+
+    // This is a valid test because the table is being erased before each test
+    expect(count).toBe(1)
+  })
+})
